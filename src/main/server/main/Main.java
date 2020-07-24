379ff7d43840187a84bd9a38b38ac7b58fbeba53
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2020-07-24 13:45:48 +0430

    Final Commit

--- src/main/server/main/Main.java
+++ src/main/server/main/Main.java
@@ -17,10 +17,6 @@ public class Main {
     public static void main(String[] args) {
         DataBase.createNewTablesToStart();
         DataBase.importAllData();
-//        TODO sort transactions
-//        System.out.println(Transaction.getAllTransactions());
-//        Transaction.sortAll();
-//        System.out.println(Transaction.getAllTransactions());
         System.out.print("Do you want to print client actions? (Y for yes, any other phrase for no)\t");
         debugPrint = scanner.nextLine().equalsIgnoreCase("Y");
         try {
